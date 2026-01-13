6013a52b3c3b072ea06030def24bcfe98b4a57c3
geng-yu <ss8611ss@hotmail.com>
2026-01-13 14:17:16 +0800

    Update day6.py

--- day6.py
+++ day6.py
@@ -16,7 +16,7 @@ def show():
     # ==========================================
     st.subheader("1️⃣ 唐吉訶德 榮本店")
     st.link_button("🐧 導航：唐吉訶德 榮本店", get_gmap_link("Don Quijote Sakae", "walking"))
-    st.markdown("[折價券(滿10,000日元)](https://japanportal.donki-global.com/coupon/cp001_zhtw.html)")
+    st.markdown("[折價券(滿10,000日元)](https://www.donki-global.com/zhtw/index.php)")
     st.divider()
 
     # ==========================================
