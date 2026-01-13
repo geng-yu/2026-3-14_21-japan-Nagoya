9040c638942b6c5d04024b7a17d36c6b5ba09878
geng-yu <ss8611ss@hotmail.com>
2026-01-13 10:24:29 +0800

    Update day7.py

--- day7.py
+++ day7.py
@@ -2,6 +2,7 @@ import streamlit as st
 from utils import get_gmap_link, show_food_table
 
 def show():
+    st.caption("1/23")
     # ==========================================
     # 1. 早餐：Konparu
     # ==========================================
