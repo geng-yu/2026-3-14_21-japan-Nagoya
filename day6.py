4e632db6f5747f70051a44a86cb698ad5defed27
geng-yu <ss8611ss@hotmail.com>
2026-01-30 08:57:09 +0800

    Update day6.py

--- day6.py
+++ day6.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import get_gmap_link, show_food_table
 
 def show():
-    st.caption("1/22")
+    st.caption("3/19")
     # ==========================================
     # 0. 早餐
     # ==========================================
