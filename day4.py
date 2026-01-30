c07726b5891e46c6869e2ed2aff21f685d8f7947
geng-yu <ss8611ss@hotmail.com>
2026-01-30 08:56:19 +0800

    Update day4.py

--- day4.py
+++ day4.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import get_gmap_link, show_food_table
 
 def show():
-    st.caption("1/20")
+    st.caption("3/17")
     # ==========================================
     # 1. 宮川朝市 (08:30)
     # ==========================================
