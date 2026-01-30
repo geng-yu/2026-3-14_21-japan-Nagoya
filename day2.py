85a535d5ca344120c54855c46ceec03c0dc4ac74
geng-yu <ss8611ss@hotmail.com>
2026-01-30 08:54:49 +0800

    Update day2.py

--- day2.py
+++ day2.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import get_gmap_link, show_food_table
 
 def show():
-    st.caption("1/18")
+    st.caption("3/15")
     # ==========================================
     # 1. 購買一日券
     # ==========================================
